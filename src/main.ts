a96e392d5a9f29d3d551cdc7c43db80e1203b070
Rodrigo Alvarez <junior@yetox.com>
2025-08-05 09:06:30 -0600

    habilitando prod

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.enableCors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://prestaapp.onrender.com', 
   credentials: true,
 });
 
